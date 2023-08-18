97093ce5301ee8b750781daecb485b336015ed61
jyehahaha <nazihaizzati@gmail.com>
2023-08-18 16:04:46 +0800

    commit message

--- itekad/settings.py
+++ itekad/settings.py
@@ -15,10 +15,7 @@ import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = os.path.dirname(PROJECT_DIR)
-BASE_DIR = os.path.dirname(os.path.dirname(os.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
