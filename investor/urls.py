c455b0c008d5857665bbc87492d170ad0cecb6ad
jyehahaha <nazihaizzati@gmail.com>
2023-08-18 16:26:24 +0800

    commit message

--- investor/urls.py
+++ investor/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.LandingPageView, name='landing_page'),
+    path('', views.DashboardPageView, name='dashboard_page'),
 ]
--- investor/views.py
+++ investor/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def DashboardPageView(request):
+    context = {}
+    return render(request, 'investor/dashboard.html', context)
