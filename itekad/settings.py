9a9f73471008e8567fc9579aedcd2eff8dafba2f
jyehahaha <nazihaizzati@gmail.com>
2023-09-11 12:14:08 +0800

    add new campaign info

--- itekad/settings.py
+++ itekad/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'itekad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join('/db', 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join('/db', 'db.sqlite3'),
+    # }
+}
 
 
 # Password validation
