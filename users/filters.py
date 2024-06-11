14381819a404e9280dc46cba9d48bd1058d2500b
Zaid Azman <matzaid@Zaids-MacBook-Pro.local>
2024-06-11 17:04:46 +0800

    Add CRUD user model

--- users/filters.py
+++ users/filters.py
@@ -1,20 +1,19 @@
 import django_filters as filters
 from django import forms
-from users.models import *
+from Application.models import *
 
 class ApplicantFilter(filters.FilterSet):
-  name = filters.CharFilter(field_name='get_full_name', label='Customer Name', lookup_expr='icontains')
-  mykad_no = filters.NumberFilter(field_name='user__mykad_no', label='Mykad No.', lookup_expr='icontains')
-  username = filters.CharFilter(field_name='username', label='Customer Name', lookup_expr='icontains')
-  email = filters.CharFilter(field_name='email', label='Email', lookup_expr='icontains')
-  bank_account_number = filters.CharFilter(field_name='user__bank_account_number', label='Account No.', lookup_expr='exact')
-  bank_name = filters.CharFilter(field_name='user__bank_name', label='Bank Name', lookup_expr='exact')
+  name = filters.CharFilter(field_name='user__name', label='Customer Name', lookup_expr='icontains')
+  mykad = filters.NumberFilter(field_name='user__mykad', label='Customer MyKad No.', lookup_expr='icontains')
   start_date = filters.DateFilter(field_name='last_modified', label='Start Date', lookup_expr='gte', widget=forms.DateInput(attrs={'type': 'date'}))
   end_date = filters.DateFilter(field_name='last_modified', label='End Date', lookup_expr='lte', widget=forms.DateInput(attrs={'type': 'date'}))
-  status = filters.ModelChoiceFilter(field_name='is_active', label='Status', lookup_expr='exact')
-  role = filters.ChoiceFilter(choices=User.role.choice, label='Role', lookup_expr='exact')
-  category = filters.ModelChoiceFilter(choices=User.position.choice, label='Category', method='exact')
+  branch = filters.ModelChoiceFilter(queryset=Branch.objects.all(), label='Branch', lookup_expr='exact')
+  stages = filters.ChoiceFilter(choices=Applicant.choice, label='Category', lookup_expr='exact')
+  product = filters.ModelChoiceFilter(queryset=ProductType.objects.all(), label='Product', method='filter_by_product')
 
   class Meta:
-    model = User
+    model = Applicant
     fields = {}
+
+  def filter_by_product(self, queryset, name, value):
+    return queryset.filter(user_financing_plan__app_type__product_type__financing_type=value)
