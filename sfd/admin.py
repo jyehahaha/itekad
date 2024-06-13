537a2b5610413943afba4751927603b9882ea0a6
Naziha Izzati <naziha.izzati@muamalat.com.my>
2024-06-13 11:03:36 +0800

    latest itekad code

--- sfd/admin.py
+++ sfd/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import CategoryOfBusiness, NatureOfBusiness
+from .models import CategoryOfBusiness, NatureOfBusiness,Campaign,CompanyProfile
 
 
 # Register your models here.
 admin.site.register(CategoryOfBusiness)
 
 admin.site.register(NatureOfBusiness)
+
+admin.site.register(Campaign)
+
+admin.site.register(CompanyProfile)
+
 
 
 
