6721bbed485f8d4c63b7839e5d9f1b15c0a482a7
zaidazman10 <matzaid@Zaids-MacBook-Pro.local>
2024-06-28 09:00:08 +0800

    add time stamp password reset dgn tukar template password reset

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordResetForm
 from .models import User, UserProfile
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class UserForm(UserCreationForm):
+    terms_agreement = forms.BooleanField(label='I accept all Privacy and Policy & Terms and Conditions.')
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2", "terms_agreement")
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
@@ -21,3 +26,12 @@ class UserProfileForm(forms.ModelForm):
             'address_line_two': "Address 2",
             'address_line_three': "Address 3",
         }
+
+class CustomPasswordResetForm(PasswordResetForm):
+    def save(self, *args, **kwargs):
+        email = self.cleaned_data["email"]
+        users = User.objects.filter(email=email)
+        for user in users:
+            user.userprofile.password_reset_timestamp = timezone.now()
+            user.userprofile.save()
+        super().save(*args, **kwargs)  
