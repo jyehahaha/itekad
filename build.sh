9b8a115a5a6e0af1412cca523bdb241fe2c2e1cf
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2024-07-12 12:37:23 +0800

    updated requirements.txt

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
+pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
-python3.9 -m pip uninstall dataclasses -f
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
