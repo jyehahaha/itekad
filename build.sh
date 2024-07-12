ab1ddadd1cf7f8a038b5f249bd01b05632e316cf
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2024-07-12 15:32:13 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 -m pip uninstall dataclasses -y
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python3 -m pip install -r requirements.txt
+python3 -m pip uninstall dataclasses -y
+python3 manage.py collectstatic --noinput --clear
+python3 manage.py makemigrations
+python3 manage.py migrate
 echo "BUILD END"
