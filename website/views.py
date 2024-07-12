aa5a1f1fd2514de8015ce8b78613d75a934db5c2
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2024-07-12 12:05:09 +0800

    fixed indent problem

--- website/views.py
+++ website/views.py
@@ -3,49 +3,49 @@ from sfd.models import Campaign, CompanyProfile,TrancheInvestor, TrancheEntrepre
 
 # Create your views here.
 def LandingPageView(request):
-    campaign_info = Campaign.objects.all()
-    context = {
-        'title': 'Home',
-        'landing': True,
-        'campaign_info' : campaign_info
-    }
-    return render(request, 'website/landing.html', context)
+	campaign_info = Campaign.objects.all()
+	context = {
+		'title': 'Home',
+		'landing': True,
+		'campaign_info' : campaign_info,
+	}
+	return render(request, 'website/landing.html', context)
 
 def AboutPageView(request):
-    context = {
-        'title': 'About Us',
-        'about': True,
-    }
-    return render(request, 'website/about.html', context)
+	context = {
+		'title': 'About Us',
+		'about': True,
+	}
+	return render(request, 'website/about.html', context)
 
 def CampaignsPageView(request):
-    campaigns = Campaign.objects.all()
-    context = {
-        'title': 'Campaigns',
-        'campaigns': True,
-        'campaigns' : campaigns
-    }
-    return render(request, 'website/campaigns.html', context)
+	campaigns = Campaign.objects.all()
+	context = {
+		'title': 'Campaigns',
+		'campaigns': True,
+		'campaigns' : campaigns,
+	}
+	return render(request, 'website/campaigns.html', context)
 
 
 def CampaignsInfoPageView(request, id):
-    campaign = Campaign.objects.get(id=id)
-    company_profile = CompanyProfile.objects.all()
-    investors = TrancheInvestor.objects.filter(campaign_id=campaign.id)
-    Entrepreneurs = TrancheEntrepreneur.objects.filter(campaign_id=campaign.id)
-    
-    context = {
-        'title': 'Campaigns',
-        'campaign' : campaign,
-        'investors' : investors,
-        'Entrepreneurs' : Entrepreneurs,
-        'company_profile' : company_profile
-    }
-    return render(request, 'website/campaign_info.html', context)
+	campaign = Campaign.objects.get(id=id)
+	company_profile = CompanyProfile.objects.all()
+	investors = TrancheInvestor.objects.filter(campaign_id=campaign.id)
+	Entrepreneurs = TrancheEntrepreneur.objects.filter(campaign_id=campaign.id)
+	
+	context = {
+		'title': 'Campaigns',
+		'campaign' : campaign,
+		'investors' : investors,
+		'Entrepreneurs' : Entrepreneurs,
+		'company_profile' : company_profile,
+	}
+	return render(request, 'website/campaign_info.html', context)
 
 def ContactPageView(request):
-    context = {
-        'title': 'Contact Us',
-        'contact': True,
-    }
-    return render(request, 'website/contact.html', context)
+	context = {
+		'title': 'Contact Us',
+		'contact': True,
+	}
+	return render(request, 'website/contact.html', context)
