236e102f27420d6b009314f7d778d1f227d8e7c7
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2024-07-12 08:51:43 +0800

    latest updated code

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 
 class User(AbstractUser):
@@ -7,7 +8,7 @@ class User(AbstractUser):
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	mykad_no = models.CharField(max_length=100, blank=True)
+	mykad_no = models.CharField(max_length=100, null=True, blank=True)
 	mobile_no = models.CharField(max_length=15, null=True, blank=True)
 	address_line_one = models.CharField(max_length=100, null=True, blank=True)
 	address_line_two = models.CharField(max_length=100, null=True, blank=True)
@@ -16,14 +17,15 @@ class UserProfile(models.Model):
 	city = models.CharField(max_length=100, null=True, blank=True)
 	state = models.CharField(max_length=100, null=True, blank=True)
 	country = models.CharField(max_length=100, null=True, blank=True)
-	bank_account_number = models.CharField(max_length=50,null=True, blank=True)
+	bank_account_number = models.CharField(max_length=50, null=True, blank=True)
 	bank_name = models.CharField(max_length=100, null=True, blank=True)
+	password_reset_timestamp = models.DateTimeField(null=True, blank=True)
 
 	class Role(models.TextChoices):
 		ADMIN = "ADMIN", 'Admin'
 		ENTREPRENEUR = "ENTREPRENEUR", 'Entrepreneur'
 		INVESTOR = "INVESTOR", 'Investor'
-		
+	
 	base_role = Role.INVESTOR
 	role = models.CharField(max_length=50, choices=Role.choices, null=True, blank=True)
 
@@ -34,8 +36,8 @@ class UserProfile(models.Model):
 	position = models.CharField(max_length=50, choices=Position.choices, null=True, blank=True)
 
 	def __str__(self):
-		return self.user.first_name + " " + self.user.last_name
-		
+		return self.user.username
+
 # Create a user Profile by default when user signs up
 def create_profile(sender, instance, created, **kwargs):
 	if created:
